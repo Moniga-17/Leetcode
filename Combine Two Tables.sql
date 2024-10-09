3b75f0d9af979ebabb112cc17e4524bb35422ddc
Moniga-17 <142727426+Moniga-17@users.noreply.github.com>
2024-10-09 15:25:27 +0530

    Update Combine Two Tables.sql

--- Combine Two Tables.sql
+++ Combine Two Tables.sql
@@ -1,6 +1,9 @@
+SQL
 //https://leetcode.com/problems/combine-two-tables/submissions/1411646390/
 
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p
 LEFT JOIN Address a
 ON p.personId = a.personId;
+
+
